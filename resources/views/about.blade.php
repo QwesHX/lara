149afa3e04e75d12bcbf92154058c3527aa4ed12
root <root@allkom.my.id>
2024-08-20 17:10:32 +0000

    Initial commit

---
+++ resources/views/about.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Halaman About</title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+<a href="/">Home</a>
+    <a href="/about">About</a>
+    <a href="/blog">Blog</a>
+    <a href="/contact">Contact</a>
+    <h1>Halaman About</h1>
+    <h1>Owner: {{ $nama }}</h1>
+    <img src="img/makise.jpg" alt="makise" width="200px">
+</body>
+</html>
---
+++ resources/views/blog.blade.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Blog</title>
+</head>
+<body>
+    <a href="/">Home</a>
+    <a href="/about">About</a>
+    <a href="/blog">Blog</a>
+    <a href="/contact">Contact</a>
+    <h1>Halaman blog</h1>
+    <article>
+        <h1> Judu1</h1>
+        <p> Lorem ipsum</p>
+    </article>
+    <article>
+        <h1> Judul 2</h1>
+        <p> Lorem ipsum</p>
+    </article>
+</body>
+</html>
